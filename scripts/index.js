29313a9efebaf8e1668b8e3cfac95bb82ee50064
Maksim <maksimkhromov95@yandex.ru>
2021-12-06 16:51:41 +0700

    v2.3

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const popupOpenProfile = document.querySelector('.profile__edit');
 const popupOpenCard = document.querySelector('.profile__add');
 const popupOpenImg = document.querySelector('.elements__photo');
 // button элемент для закрытия попапа
-const PopupClose = document.querySelector('.popup__close');
+const popupClose = document.querySelector('.popup__close');
 const closePopupProfile = document.querySelector('.popup__close-profile');
 const closePopupCard = document.querySelector('.popup__close-card');
 const closePopupImg = document.querySelector('.popup__close-img');
@@ -24,22 +24,33 @@ const inputName = document.getElementById('namenew');
 const inputLink = document.getElementById('link');
 const popupImg = document.querySelector('.popupimg__img');
 const popupImgText = document.querySelector('.popupimg__text');
-const popupImgClose = document.querySelector('.popup__close');
 
 function openIsPopup(popup) {
     popup.classList.add('popup_is-opened');
-    closesPopupUseEsc(popup);
+    const saveButtonElements = Array.from(document.querySelectorAll('.popup__save'));
+    saveButtonElements.forEach(button => {
+        button.classList.add('popup__save_state_invalid');
+        button.disabled = true; 
+    })
+    removePopupUseEsc(popup);
 }
 
-
-function closesPopupUseEsc(popup) {
+function removePopupUseEsc(popup) {
     document.addEventListener('keydown', function (evt) {
         const esc = 27;
         if (evt.keyCode === esc) {
             closesPopup(popup);
         }
-    document.removeEventListener('keydown', closesPopupUseEsc);
+    removeEventListener(popup);
     });
+}
+
+function closesPopup(popup) {
+    popup.classList.remove('popup_is-opened');
+};
+
+function removeEventListener(popup) {
+    document.removeEventListener('keydown', removePopupUseEsc);
 }
 
 function closesPopupClickOverlay(evt) {
@@ -49,15 +60,10 @@ function closesPopupClickOverlay(evt) {
     }
 };
 
-function closesPopup(popup) {
-    popup.classList.remove('popup_is-opened');
-};
-
 function removePopupClick(evt) {
     const opensPopup = evt.target.closest('.popup_is-opened');
     closesPopup(opensPopup);
 };
-
 
 const savePopup = function (event) {
     event.preventDefault();
@@ -132,13 +138,12 @@ popupOpenProfile.addEventListener('click', () => {
     openIsPopup(popupElementProfile);
 });
 
-const saveCard = document.getElementById('buttonsavecard');
 popupOpenCard.addEventListener('click', () => {
     inputName.value = '';
     inputLink.value = '';
-    // saveCard.setAttribute('disabled', true);
     openIsPopup(popupElementCard);
 });
+
 
 closePopupProfile.addEventListener('click', removePopupClick);
 closePopupCard.addEventListener('click', removePopupClick);
